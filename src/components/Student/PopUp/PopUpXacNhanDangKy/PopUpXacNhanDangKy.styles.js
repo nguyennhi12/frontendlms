92a97f3f7dd154e0ecaaff3ea1f7b05c741cb56d
18110121 <18110121@student.hcmute.edu.vn>
2021-09-15 18:09:28 +0700

    update

--- src/components/Student/PopUp/PopUpXacNhanDangKy/PopUpXacNhanDangKy.styles.js
+++
@@ -1,4 +0,0 @@
-import styled from "styled-components";
-export const Wrapper = styled.body `
-   background:#055031;
-`;
--- src/components/Student/PopUp/PopUpXacNhanHuy/PopUpXacNhanDangKy.styles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-import NenXN from '../../../../images/geometry-polygon-purple-violet-wallpaper-preview.jpg'
-export const Wrapper = styled.body `
-    background: url(${NenXN});
-`;
